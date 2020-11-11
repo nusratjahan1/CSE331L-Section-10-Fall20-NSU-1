74e30587209942ba185df640ee9809fd2301bc54
nusratjahan1 <50940337+nusratjahan1@users.noreply.github.com>
2020-11-11 10:18:04 +0600

    lab1

---
+++ Lab Assessment Submission/Lab 1/lab 1 assesment.asm
@@ -0,0 +1,19 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h   
+
+MOV AX,01
+MOV AX,05
+MOV AX,05
+ADD AX,AX
+
+
+; add your code here
+
+ret
+
+
+
+
